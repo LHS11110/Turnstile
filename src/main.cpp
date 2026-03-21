ba331b607e999e75f769cdffce9d7c3cee594c84
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2026-03-22 00:27:20 +0900

    Remove lexer test code from main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,5 @@
 int main() {
   std::cout << "Turnstile: System LK Proof Assistant Lexer Test\n\n";
 
-  std::string source = "theorem P -> P;\naxiom A;\nforall x. P(x)";
-  Lexer lexer(source);
-  std::vector<Token> tokens = lexer.tokenize();
-
-  std::cout << "Tokens:\n";
-  for (const auto &token : tokens) {
-    std::cout << "  " << tokenTypeToString(token.type) << "\t" << token.value
-              << "\t(" << token.line << ", " << token.column << ")\n";
-  }
-
   return 0;
 }
